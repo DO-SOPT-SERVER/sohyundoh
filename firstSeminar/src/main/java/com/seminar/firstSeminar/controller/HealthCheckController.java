2f11ba5dc3de45aec8381e6f0c99a96ff8a169aa
sohyundoh <lyny.doh@gmail.com>
2023-10-09 21:36:09 +0900

    #1 [feat] 1주차 과제 제출

---
+++ firstSeminar/src/main/java/com/seminar/firstSeminar/controller/HealthCheckController.java
@@ -0,0 +1,47 @@
+package com.seminar.firstSeminar.controller;
+
+import com.seminar.firstSeminar.common.dto.SuccessResponse;
+import com.seminar.firstSeminar.dto.HealthCheckResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/health")
+public class HealthCheckController {
+
+    @GetMapping("/v1")
+    public Map<String, String> healthCheck() {
+        Map<String, String> response = new HashMap<>();
+        response.put("status", "OK");
+        return response;
+    }
+
+    @GetMapping("/v2")
+    public ResponseEntity<String> healthCheckV2() {
+        return ResponseEntity.ok("OK");
+    }
+
+
+    @GetMapping("/v4")
+    public ResponseEntity<Map<String, String>> healthCheckV4() {
+        Map<String, String> response = new HashMap<>();
+        response.put("status", "OK");
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/v5")
+    public ResponseEntity<HealthCheckResponse> healthCheckV5() {
+        return ResponseEntity.ok(new HealthCheckResponse("success"));
+    }
+
+    @GetMapping("/v6")
+    public SuccessResponse healthCheckV6() {
+        return SuccessResponse.success("200", "Success", true);
+    }
+
+}
